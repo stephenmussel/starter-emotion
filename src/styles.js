7b1deed29f68a193e0ca7947e11f3048e0971f54
phaydara <phaydara@gmail.com>
2022-06-23 21:27:22 -0500

    added CardWrapper component

--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,13 @@
 // create and export styled components along with the animation and theming
 import styled from "@emotion/styled";
 
+export const CardWrapper = styled.div`
+    width: 250px;
+    height: 325;
+    background: #fff;
+    border-radius: 15px;
+    padding-bottom: 5px;
+    @media (max-width: 900px) {
+        width: 400px;
+    }
+`;
